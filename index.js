94a997b638604c636b4bbc30acc4b002d8847dd9
Snehal490102 <2022.snehal.patil@ves.ac.in>
2024-08-18 19:55:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+// index.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hey Snehal,Here is the Dynamic Website.")
-})
+app.get('/', (req, res) => {
+  res.send('Hello Snehal,this is dynamic hosting !');
+});
 
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
