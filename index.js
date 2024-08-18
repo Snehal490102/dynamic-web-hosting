0556272abef42c46bbc2836f36802c4fe21fbdb7
Snehal490102 <2022.snehal.patil@ves.ac.in>
2024-08-18 15:19:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hey Snehal,Here is the Dynamic Website.")
+})
+
+app.get('/login',(req,res)=>{
+    res.send("Login Here.")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
